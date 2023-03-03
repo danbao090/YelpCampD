aead974c8f14b7ba58f5ea093179f6372a51538a
Di Sang <dumblesong@gmail.com>
2023-03-03 12:49:23 +0100

    adjust code

--- app.js
+++ app.js
@@ -19,11 +19,9 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-
 const MongoStore = require("connect-mongo")(session);
 
-
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl =  process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -50,10 +48,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 const store = new MongoStore({
     url: dbUrl,
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     touchAfter: 24 * 60 * 60
 });
 
@@ -61,12 +60,10 @@ store.on("error", function (e) {
     console.log("SESSION STORE ERROR", e)
 })
 
-
-
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -166,7 +163,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
